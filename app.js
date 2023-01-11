572967630782d7a4e596e12962bc372f6a24b536
sebastien baudet <sebastien.baudet@selhagroup.com>
2023-01-11 10:09:15 +0100

    first commit*

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+  axios.get('https://pokeapi.co/api/v2/pokemon/ditto/')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
